b1a8e706a5854cd0ab7e6821a5d5b319e322ac56
Meheret Egzerab <meheretgirma22@gmail.com>
2017-01-11 20:41:32 -0500

    layout

--- server.js
+++ server.js
@@ -27,6 +27,73 @@ mongoose.connect(config.database, function(err) {
     console.log("Successfully connected to the database");
 })
 
+var routes = require("./routes/index");
+var users = require("./routes/user");
+
+//View Engine
+
+app.set("views", path.join(__dirname, "views"));
+app.engine("handlebars", exphbs({
+    defaultLayout: "layout"
+}));
+app.set("view engine", "handlebars");
+
+//BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Express session
+
+app.use(session({
+    secrect: "secret",
+    saveUnitialized: true,
+    resave: true
+}));
+
+//Passport intit
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+//Express Validator
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.'),
+            root = namespace.shift(),
+            formParam = root;
+
+        while (namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };
+    }
+}));
+
+//connect flash
+
+app.use(flash());
+
+app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash("success_msg");
+    res.locals.error_msg = req.flash("error_msg");
+    res.locals.error = req.flash("error");
+    next();
+})
+
+app.use("/", routes);
+app.use("/user", users);
+
+
 app.listen(port, function() {
     console.log("Server is listening on port" + port)
 })
